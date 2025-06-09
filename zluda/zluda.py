4788bde455fa1b655a12aa1abad49781778a418c
1635200412 <84870955+1635200412@users.noreply.github.com>
2025-06-09 13:22:48 +0800

    Update zluda.py

--- zluda/zluda.py
+++ zluda/zluda.py
@@ -1,21 +1,44 @@
 import torch
 
 
-
+debug=False
 def angto(input,dt):
     if isinstance(input, torch.Tensor):
-        if torch.is_complex(input):
-            real_part_bf16 = input.real.to(dtype=dt)
-            imag_part_bf16 = input.imag.to(dtype=dt)
-            return torch.complex(real_part_bf16, imag_part_bf16)
-        else:
-            return input.to(dtype=dt)
+        if dt==torch.float32:
+            if torch.is_complex(input):
+                real_part_bf16 = input.real.to(dtype=dt)
+                imag_part_bf16 = input.imag.to(dtype=dt)
+                return torch.complex(real_part_bf16, imag_part_bf16)
+            else:
+                return input.to(dtype=dt)
+        if dt==torch.float16:
+            if torch.is_complex(input):
+                real_part_bf16 = input.real.to(dtype=dt)
+                imag_part_bf16 = input.imag.to(dtype=dt)
+                return torch.complex(real_part_bf16, imag_part_bf16)
+            else:
+                return input.to(dtype=dt)
+        if dt==torch.bfloat16:
+            if torch.is_complex(input):
+                real_part_bf16 = input.real.to(dtype=dt)
+                imag_part_bf16 = input.imag.to(dtype=dt)
+                return torch.complex(real_part_bf16, imag_part_bf16)
+            else:
+                return input.to(dtype=dt)
+
     return input
 
 
 _topk = torch.topk
-def topk(input: torch.Tensor, *args, **kwargs): 
+def topk(input: torch.Tensor, *args, **kwargs):
     device = input.device
+    dt=input.dtype
+    if debug:
+        print(f"topk:device{device},dtype:{dt}")
+    if input.dtype != torch.float32:
+        input=angto(input,torch.float32)
+        values, indices = _topk(input.cpu(), *args, **kwargs)
+        return torch.return_types.topk((values.to(device).to(dtype=dt), indices.to(device).to(dtype=dt),))
     values, indices = _topk(input.cpu(), *args, **kwargs)
     return torch.return_types.topk((values.to(device), indices.to(device),))
 
@@ -41,14 +64,25 @@ def jit_script(f, *_, **__):
 _torch_sinc= torch.sinc
 def torch_sinc(input,*args,**kwargs):
     #print("sinc")
+    device = input.device
+    dt=input.dtype
+    if debug:
+        print(f"sinc:device{device},dtype:{dt}")
     return _torch_sinc(input.cpu(),*args,**kwargs).to(input.device)
 
 _torch_exp=torch.exp
 def torch_exp(input,*args,**kwargs): 
     #print("exp")
+    device = input.device
+    dt=input.dtype
+    if debug:
+        print(f"exp:device{device},dtype:{dt}")
     if input.dtype != torch.float32:
         input=angto(input,torch.float32)
-        return angto(_torch_exp(input.cpu(),*args,**kwargs).to(input.device),torch.float32)
+        tmp=_torch_exp(input.cpu(),*args,**kwargs).to(device)
+        if debug:
+            print(f"exp:device{device},return：dtype:{tmp.dtype}")
+        return angto(tmp,dt)
     kwargs = {k: v.cpu() if isinstance(v, torch.Tensor) else v for k, v in kwargs.items()}
     return _torch_exp(input.cpu(),*args,**kwargs).to(input.device)
 
@@ -56,17 +90,27 @@ def torch_exp(input,*args,**kwargs):
 _stft = torch.stft
 def stft_fftn(input,*args, **kwargs): # pylint: disable=redefined-builtin
     # print("使用stft")
+    device = input.device
+    dt=input.dtype
+    if debug:
+        print(f"stft:device{device},dtype:{dt}")
     if input.dtype != torch.float32:
         input=angto(input,torch.float32)
         kwargs = {k: angto(v.cpu(),torch.float32) if isinstance(v, torch.Tensor) else v for k, v in kwargs.items()}
+        return angto(_stft(input.cpu(),*args,**kwargs).to(device),dt)
     kwargs = {k: v.cpu() if isinstance(v, torch.Tensor) else v for k, v in kwargs.items()}
     return _stft(input.cpu(),*args,**kwargs).to(input.device)
 
 _istft = torch.istft
 def istft_fftn(input, *args, **kwargs,): 
+    device = input.device
+    dt=input.dtype
+    if debug:
+        print(f"istft:device{device},dtype:{dt}")
     if input.dtype != torch.float32:
         input=angto(input,torch.float32)
         kwargs["window"]=angto(kwargs["window"].cpu(),torch.float32)
+        return angto(_istft(input.cpu(),*args,**kwargs).to(device),dt)
         #kwargs = {k: angto(v.cpu(),torch.float32) if isinstance(v, torch.Tensor) else v for k, v in kwargs.items()}
     kwargs = {k: v.cpu() if isinstance(v, torch.Tensor) else v for k, v in kwargs.items()}
     return _istft(input.cpu(),*args,**kwargs).to(input.device)
@@ -74,17 +118,27 @@ def istft_fftn(input, *args, **kwargs,):
 
 _fft_irfft= torch.fft.irfft
 def fft_irfft(input:torch.Tensor,*args, **kwargs)->torch.Tensor: 
+    device = input.device
+    dt=input.dtype
+    if debug:
+        print(f"irfft:device{device},dtype:{dt}")
     if input.dtype != torch.float32:
         input=angto(input,torch.float32)
         kwargs = {k: angto(v.cpu(),torch.float32) if isinstance(v, torch.Tensor) else v for k, v in kwargs.items()}
+        return angto(_fft_irfft(input.cpu(),*args, **kwargs).to(device),dt)
     kwargs = {k: v.cpu() if isinstance(v, torch.Tensor) else v for k, v in kwargs.items()}
     return _fft_irfft(input.cpu(),*args, **kwargs).to(input.device)
 
 _fft_rfft=torch.fft.rfft
 def fft_rfft(input:torch.Tensor,*args,**kwargs)->torch.Tensor:
+    device = input.device
+    dt=input.dtype
+    if debug:
+        print(f"rfft:device{device},dtype:{dt}")
     if input.dtype != torch.float32:
         input=angto(input,torch.float32)
-        kwargs = {k: angto(v.cpu(),torch.float32) if isinstance(v, torch.Tensor) else v for k, v in kwargs.items()}
+        kwargs = {k: angto(v.cpu,torch.float32) if isinstance(v, torch.Tensor) else v for k, v in kwargs.items()}
+        return angto(_fft_rfft(input.cpu(), *args,**kwargs).to(device),dt)
     kwargs = {k: v.cpu() if isinstance(v, torch.Tensor) else v for k, v in kwargs.items()}
     return _fft_rfft(input.cpu(), *args,**kwargs).to(input.device)
     
@@ -94,17 +148,20 @@ def Data_Loader(*args,**kwargs):
     return _Data_Loader(*args,**kwargs)
 
 
+_orig_torch_einsum = torch.einsum
+
+def torch_einsum(equation, *operands):
+    device=operands[0].device
+    operands_on_cpu = [operand.cpu() for operand in operands]
+    result = _orig_torch_einsum(equation, *operands_on_cpu)
+    return result.to(device)
 
 
-def do_hijack():
-    #print("do_hijack")
+def do_hijack(gfx):
+    
     torch.version.hip = 5.7
-
     torch.topk = topk
     torch.jit.script = jit_script
-    #torch.fft.fftn = fft_fftn
-    #torch.fft.ifftn = fft_ifftn
-    #torch.fft.rfftn = fft_rfftn
     torch.stft=stft_fftn
     torch.istft=istft_fftn
     torch.fft.irfft=fft_irfft
@@ -112,6 +169,8 @@ def do_hijack():
     torch.sinc=torch_sinc
     torch.exp=torch_exp
     torch.utils.data.DataLoader=Data_Loader
+    if gfx=="gfx803":
+        torch.einsum=torch_einsum
 
 
 def de_dnn():
@@ -135,12 +194,8 @@ def de_dnn():
         torch.backends.cuda.enable_mem_efficient_sdp = do_nothing
 
 
-
-def initialize_zluda():
-
-
-
-    do_hijack()
+def initialize_zluda(gfx):
+    do_hijack(gfx)
     de_dnn()
 
 

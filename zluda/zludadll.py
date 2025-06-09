0deeff27acdd94cd4c7925b0778d4af4af10abd9
1635200412 <84870955+1635200412@users.noreply.github.com>
2025-06-09 13:22:29 +0800

    Update zludadll.py

--- zluda/zludadll.py
+++ zluda/zludadll.py
@@ -2,10 +2,13 @@
 import os
 import ctypes
 
-
-ZLUDA_TARGETS = ('nvcuda.dll',)# 'nvml.dll',"nccl.dll")
+from pathlib import Path
+AMD_DLL=['amdhip64.dll']
+HIPSDK_TARGETS = ['rocblas.dll']
+ZLUDA_TARGETS = ('nvcuda.dll',)#"nvml.dll","nccl.dll")#"nccl.dll",)# 'nvml.dll')#,"nccl.dll")
 DLL_MAPPING = {
     'cublas.dll': 'cublas64_11.dll',
+    #'cudnn.dll':'cudnn64_8.dll',
     #'cudart.dll':'cudart64_110.dll',
     #'cusparse.dll':'cusparse64_11.dll',
     #'cufft.dll': 'cufft64_10.dll',
@@ -14,20 +17,24 @@ DLL_MAPPING = {
     }
 
 
-def load_dll() -> None:
-    os.environ["ZLUDA_COMGR_LOG_LEVEL"] = "1"
-    zluda_path=".\zluda\\bin"
+def load_dll(gfx) -> None:
 
-    # 打印 DLL 文件的路径
+    os.environ["ZLUDA_COMGR_LOG_LEVEL"] = "1"
+    zluda_path=os.path.join(Path(__file__).parent, "bin")
+    
+    if gfx=="gfx803":
+        os.environ["ROCBLAS_TENSILE_LIBPATH"] = os.path.join(Path(__file__).parent, "bin","rocblas","library")+"\\"
+        dll_580=os.path.join(Path(__file__).parent, "bin","580")
+        for v in AMD_DLL:
+            ctypes.CDLL(os.path.join(zluda_path, v),mode=ctypes.RTLD_GLOBAL)
+        for v in HIPSDK_TARGETS:
+            ctypes.CDLL(os.path.join(dll_580, v),mode=ctypes.RTLD_GLOBAL)
+        
 
     for v in ZLUDA_TARGETS:
         ctypes.CDLL(os.path.join(zluda_path, v),mode=ctypes.RTLD_GLOBAL)
-        #ctypes.windll.LoadLibrary(os.path.join(zluda_path, v))
-
     for v in DLL_MAPPING.values():
-        #ctypes.windll.LoadLibrary(os.path.join(zluda_path, v))
         ctypes.CDLL(os.path.join(zluda_path, v),mode=ctypes.RTLD_GLOBAL)
-
 
 
 
